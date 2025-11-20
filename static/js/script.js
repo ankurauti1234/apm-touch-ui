193f49ccb6cf77a25bcbc492532a68484fe9a834
PranavDalve <pranavudalve@gmail.com>
2025-11-20 17:32:56 +0530

    UI keyboard changes

--- static/js/script.js
+++ static/js/script.js
@@ -60,14 +60,9 @@ const states = {
         <p>Begin the installation process for your meter system.</p>
         <div class="separator"></div>
         <div class="button-group">
-        <input type="text" id="hhid" value="HH" maxlength="6" placeholder="Enter HHID (e.g. HH1002)" oninput="this.value = this.value.replace(/[^a-zA-Z0-9]/g, '').toUpperCase()" onfocus="showKeyboard(this)">
-
-        <button class="button secondary" onclick="navigate('hhid_input')">
-                <span class="material-icons">replay</span> Retry OTP
-            </button>
-            <button class="button" onclick="navigate('connect_select')">
-                <span class="material-icons">play_arrow</span> Start Installation
-            </button>
+        <button class="button" onclick="navigate('connect_select')">
+            <span class="material-icons">play_arrow</span> Start Installation
+        </button>
         </div>`,
 
     connect_select: (currentSSID = null) => `
