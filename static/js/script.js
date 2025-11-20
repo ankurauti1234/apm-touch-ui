f15a31e7815acdb696033f1c4a947e6ea24a22cc
PranavDalve <pranavudalve@gmail.com>
2025-11-20 16:27:33 +0530

    UI keyboard changes

--- static/js/script.js
+++ static/js/script.js
@@ -63,7 +63,7 @@ const states = {
         <input type="text" id="hhid" value="HH" maxlength="6" placeholder="Enter HHID (e.g. HH1002)" oninput="this.value = this.value.replace(/[^a-zA-Z0-9]/g, '').toUpperCase()" onfocus="showKeyboard(this)">
 
         <button class="button secondary" onclick="navigate('hhid_input')">
-                <span class="material-icons">arrow_back</span> Retry OTP
+                <span class="material-icons">replay</span> Retry OTP
             </button>
             <button class="button" onclick="navigate('connect_select')">
                 <span class="material-icons">play_arrow</span> Start Installation
