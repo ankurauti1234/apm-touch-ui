a900c35f10903ed698fe1853ce02bd3e1fa1fdd7
PranavDalve <pranavudalve@gmail.com>
2025-12-30 14:33:56 +0530

    Fixing error for boot event

--- src/boot_manager.py
+++ src/boot_manager.py
@@ -4,7 +4,7 @@ import sqlite3
 import threading
 from src.config import DB_PATH, METER_ID  # Adjust import based on your project structure
 from src.config import load_hhid
-from users.members import load_members_data
+from src.users.members import load_members_data
 from mqtt import _enqueue, _pub_q, _q_lock, calculate_age  # Assuming you have a mqtt module
 
 LAST_BOOT_ID_FILE = "/var/lib/meter_last_boot_id.txt"
