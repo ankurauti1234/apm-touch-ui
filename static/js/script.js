40cfd0cf3b4b5acd292e8f5621e248a159e31221
PranavDalve <pranavudalve@gmail.com>
2026-01-21 18:51:59 +0400

    Feature Adding member icons which are active in screensaver

--- static/js/script.js
+++ static/js/script.js
@@ -2489,7 +2489,7 @@ setupScreensaverResetListeners();  // call once
        warningContainer.innerHTML = `
            <span class="material-icons" style="font-size:80px; color:#e74c3c;">warning_amber</span>
            <div style="margin-top:20px; font-size:25px; color:#e74c3c;">No active members</div>
-           <div style="margin-top:10px; font-size:20px; color:#bbb; max-width:80%; text-align:center;">
+           <div style="margin-top:5px; font-size:20px; color:#bbb; max-width:80%; text-align:center;">
                Please activate members in the dashboard
            </div>
        `;
