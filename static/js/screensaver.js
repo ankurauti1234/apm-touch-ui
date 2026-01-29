5df61960d74d1394692c2ecf748d9de37fb4157d
PranavDalve <pranavudalve@gmail.com>
2026-01-29 17:02:48 +0530

    Added the wheather functionality for testing

--- static/js/screensaver.js
+++ static/js/screensaver.js
@@ -117,13 +117,26 @@
            const temp = Math.round(current.temperature_2m);
            const weatherCode = current.weather_code;
    
-           let icon = '🌤️';
-           let condition = 'Clear';
-           if (weatherCode >= 0 && weatherCode <= 3) { icon = `/static/assets/sunny.png`; condition = 'Sunny'; }
-           else if (weatherCode <= 48) { icon = '☁️'; condition = 'Cloudy'; }
-           else if (weatherCode <= 67) { icon = '🌧️'; condition = 'Rain'; }
-           else if (weatherCode <= 77) { icon = '❄️'; condition = 'Snow'; }
-           else if (weatherCode <= 99) { icon = '⛈️'; condition = 'Thunderstorm'; }
+           let icon = '';
+            let condition = '';
+
+            if (weatherCode >= 0 && weatherCode <= 3) {
+            icon = 'https://uxwing.com/wp-content/themes/uxwing/download/weather-weather/sunny-icon.png';
+            condition = 'Sunny';
+            } else if (weatherCode <= 48) {
+            icon = 'https://uxwing.com/wp-content/themes/uxwing/download/weather-weather/cloudy-icon.png';
+            condition = 'Cloudy';
+            } else if (weatherCode <= 67) {
+            icon = 'https://uxwing.com/wp-content/themes/uxwing/download/weather-weather/rain-icon.png';
+            condition = 'Rain';
+            } else if (weatherCode <= 77) {
+            icon = 'https://uxwing.com/wp-content/themes/uxwing/download/weather-weather/snow-icon.png';
+            condition = 'Snow';
+            } else if (weatherCode <= 99) {
+            icon = 'https://uxwing.com/wp-content/themes/uxwing/download/weather-weather/thunderstorm-icon.png';
+            condition = 'Thunderstorm';
+            }
+
    
            const weatherEl = document.getElementById('weather-status');
            weatherEl.innerHTML = `
