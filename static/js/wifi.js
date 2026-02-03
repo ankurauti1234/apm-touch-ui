dc8b33a8c57a9b311e3e0742a9c17b7eadfe316e
PranavDalve <pranavudalve@gmail.com>
2026-02-03 10:18:02 +0530

    adding code for testing

--- static/js/wifi.js
+++ static/js/wifi.js
@@ -384,7 +384,7 @@ if (!document.getElementById('wifi-spinner-style')) {
             align-items: center;
             gap: 12px;
             font-weight: 500;
-            max-width: 90%;
+            max-width: 100px;
         `;
     
         banner.innerHTML = `
