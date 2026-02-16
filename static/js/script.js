1e6c8e80d7e7bddd5bcd478c383422641a627903
PranavDalve <pranavudalve@gmail.com>
2026-02-16 19:08:07 +0530

    making a popup

--- static/js/script.js
+++ static/js/script.js
@@ -2456,7 +2456,6 @@ function getScreensaverContent() {
                     height: 110px;
                     border-radius: 50%;
                     background: center/cover url('${avatar(m.gender, m.dob)}') no-repeat;
-                    border: 4px solid #444;
                     box-shadow: 0 8px 24px rgba(0,0,0,0.7);
                 "></div>
                 <div style="
