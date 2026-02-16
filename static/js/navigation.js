5524a9afd732b06ad4d9f63dea85abe71c55ec44
PranavDalve <pranavudalve@gmail.com>
2026-02-16 12:08:48 +0530

    Adding a function that send hourly member events

--- static/js/navigation.js
+++ static/js/navigation.js
@@ -40,6 +40,12 @@ async function navigate(state, param = null) {
         return;
     }
 
+    if (state === 'otp_verification') {
+        render();
+        setTimeout(startInputSourceRetry, 800);
+        return;
+    }
+
     /* ---------- INPUT SOURCE DETECTION ---------- */
     if (state === 'input_source_detection') {
         render();
